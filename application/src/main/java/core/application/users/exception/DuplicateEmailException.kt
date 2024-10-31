af404617c447b03188ea324e5f21b80d54750a3b
minjung <minjung0768@gmail.com>
2024-10-31 12:24:19 +0900

    refactor : user domain error kotlin migration

---
+++ application/src/main/java/core/application/users/exception/DuplicateEmailException.kt
@@ -0,0 +1,11 @@
+package core.application.users.exception
+
+class DuplicateEmailException : RuntimeException {
+    constructor() : super()
+
+    constructor(message: String?) : super(message)
+
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+
+    constructor(cause: Throwable?) : super(cause)
+}
