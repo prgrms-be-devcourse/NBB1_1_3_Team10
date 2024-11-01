489e962250ceed9517a0b6a6f6e0cf73d370af58
seonghoon <gortmtjdgns0943@gmail.com>
2024-11-01 12:26:32 +0900

    Movie Service Kotlin 마이그레이션

--- application/src/main/java/core/application/movies/service/CommentService.kt
+++ application/src/main/java/core/application/movies/service/CommentService.kt
@@ -1,134 +1,139 @@
-package core.application.movies.service;
+package core.application.movies.service
 
-import java.util.UUID;
-
-import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import core.application.movies.constant.CommentSort;
-import core.application.movies.exception.InvalidReactionException;
-import core.application.movies.exception.InvalidWriteCommentException;
-import core.application.movies.exception.NoMovieException;
-import core.application.movies.exception.NotCommentWriterException;
-import core.application.movies.exception.NotFoundCommentException;
-import core.application.movies.exception.NotMatchMovieCommentException;
-import core.application.movies.models.dto.request.CommentWriteReqDTO;
-import core.application.movies.models.dto.response.CommentRespDTO;
-import core.application.movies.models.entities.CachedMovieEntity;
-import core.application.movies.models.entities.CommentEntity;
-import core.application.movies.repositories.comment.CommentDislikeRepository;
-import core.application.movies.repositories.comment.CommentLikeRepository;
-import core.application.movies.repositories.comment.CommentRepository;
-import core.application.movies.repositories.movie.CachedMovieRepository;
-import core.application.users.models.entities.UserEntity;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import core.application.movies.constant.CommentSort
+import core.application.movies.exception.*
+import core.application.movies.models.dto.request.CommentWriteReqDTO
+import core.application.movies.models.dto.response.CommentRespDTO
+import core.application.movies.models.dto.response.CommentRespDTO.Companion.of
+import core.application.movies.models.entities.CommentEntity.Companion.of
+import core.application.movies.repositories.comment.CommentDislikeRepository
+import core.application.movies.repositories.comment.CommentLikeRepository
+import core.application.movies.repositories.comment.CommentRepository
+import core.application.movies.repositories.movie.CachedMovieRepository
+import core.application.users.models.entities.UserEntity
+import java.util.UUID
+import org.slf4j.LoggerFactory
+import org.springframework.data.domain.Page
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.function.Supplier
 
 @Service
-@Slf4j
-@RequiredArgsConstructor
-public class CommentService {
-	private final CachedMovieRepository movieRepository;
-	private final CommentRepository commentRepository;
-	private final CommentLikeRepository likeRepository;
-	private final CommentDislikeRepository dislikeRepository;
+class CommentService(
+    private val movieRepository: CachedMovieRepository,
+    private val commentRepository: CommentRepository,
+    private val likeRepository: CommentLikeRepository,
+    private val dislikeRepository: CommentDislikeRepository
+) {
 
-	@Transactional(readOnly = true)
-	public Page<CommentRespDTO> getComments(String movieId, int page, CommentSort sort, UUID userId) {
-		return switch (sort) {
-			case LIKE -> commentRepository.findByMovieIdOnLikeDescend(movieId, userId, page);
-			case LATEST -> commentRepository.findByMovieIdOnDateDescend(movieId, userId, page);
-			default -> commentRepository.findByMovieIdOnDislikeDescend(movieId, userId, page);
-		};
-	}
+    private val log = LoggerFactory.getLogger(CommentService::class.java)
 
-	@Transactional
-	public CommentRespDTO writeCommentOnMovie(CommentWriteReqDTO writeReqDTO, UserEntity user, String movieId) {
-		// 이미 작성한 기록이 있는지 확인한다.
-		log.info("user = {}", user);
-		if (commentRepository.existsByMovieIdAndUserId(movieId, user.getUserId())) {
-			throw new InvalidWriteCommentException("한줄평은 1회 작성만 가능합니다.");
-		}
-		CommentEntity newComment = CommentEntity.of(writeReqDTO, movieId, user.getUserId());
-		CommentEntity save = commentRepository.saveNewComment(movieId, user.getUserId(), newComment);
-		CachedMovieEntity movie = movieRepository.findByMovieId(movieId)
-			.orElseThrow(() -> new NoMovieException("존재하지 않는 영화입니다."));
-		log.info("수정 전 영화 총 평점 : {}, 수정 전 영화 한줄평 개수 : {}", movie.getSumOfRating(), movie.getCommentCount());
-		movie.isCommentedWithRating(newComment.getRating());
-		log.info("수정된 영화 총 평점 : {}, 수정된 영화 한줄평 개수 : {}", movie.getSumOfRating(), movie.getCommentCount());
-		movieRepository.editMovie(movieId, movie);
-		return CommentRespDTO.of(save, user.getAlias());
-	}
+    @Transactional(readOnly = true)
+    fun getComments(movieId: String?, page: Int, sort: CommentSort, userId: UUID?): Page<CommentRespDTO?>? {
+        return when (sort) {
+            CommentSort.LIKE -> commentRepository.findByMovieIdOnLikeDescend(movieId, userId, page)
+            CommentSort.LATEST -> commentRepository.findByMovieIdOnDateDescend(movieId, userId, page)
+            else -> commentRepository.findByMovieIdOnDislikeDescend(movieId, userId, page)
+        }
+    }
 
-	@Transactional
-	public void deleteCommentOnMovie(String movieId, UUID userId, Long commentId) {
-		CommentEntity comment = commentRepository.findByCommentId(commentId)
-			.orElseThrow(() -> new NotFoundCommentException("존재하지 않는 한줄평입니다."));
-		if (!comment.getUserId().equals(userId)) {
-			throw new NotCommentWriterException("한줄평 작성자가 아닙니다.");
-		}
-		if (!comment.getMovieId().equals(movieId)) {
-			throw new NotMatchMovieCommentException("해당 영화의 한줄평이 아닙니다.");
-		}
+    @Transactional
+    fun writeCommentOnMovie(writeReqDTO: CommentWriteReqDTO, user: UserEntity, movieId: String): CommentRespDTO {
+        // 이미 작성한 기록이 있는지 확인한다.
+        log.info("user = {}", user)
 
-		commentRepository.deleteComment(commentId);
-		CachedMovieEntity movie = movieRepository.findByMovieId(movieId)
-			.orElseThrow(() -> new NoMovieException("존재하는 영화가 아닙니다."));
-		log.info("[MovieService.deleteCommentOnMovie] 영화 정보 수정");
-		log.info("[MovieService.deleteCommentOnMovie] before rating : {}, commentCount : {}", movie.getSumOfRating(),
-			movie.getCommentCount());
-		movie.deleteComment(comment.getRating());
-		log.info("[MovieService.deleteCommentOnMovie] before rating : {}, commentCount : {}", movie.getSumOfRating(),
-			movie.getCommentCount());
-		movieRepository.editMovie(movieId, movie);
-	}
+        if (commentRepository.existsByMovieIdAndUserId(movieId, user.userId) == true) {
+            throw InvalidWriteCommentException("한줄평은 1회 작성만 가능합니다.")
+        }
+        val newComment = of(writeReqDTO, movieId, user.userId)
+        val save = commentRepository.saveNewComment(movieId, user.userId, newComment)
 
-	@Transactional
-	public void incrementCommentLike(Long commentId, UUID userId) {
-		CommentEntity comment = commentRepository.findByCommentId(commentId)
-			.orElseThrow(() -> new NotFoundCommentException("존재하지 않는 한줄평입니다."));
-		if (likeRepository.isExistLike(commentId, userId)) {
-			throw new InvalidReactionException("이미 '좋아요'를 누른 한줄평입니다.");
-		}
-		comment.isLiked();
-		commentRepository.update(comment);
-		likeRepository.saveCommentLike(commentId, userId);
-	}
+        val movie = movieRepository.findByMovieId(movieId)
+            ?.orElseThrow(Supplier { NoMovieException("존재하지 않는 영화입니다.") })
 
-	@Transactional
-	public void decrementCommentLike(Long commentId, UUID userId) {
-		CommentEntity comment = commentRepository.findByCommentId(commentId)
-			.orElseThrow(() -> new NotFoundCommentException("존재하지 않는 한줄평입니다."));
-		if (!likeRepository.isExistLike(commentId, userId)) {
-			throw new InvalidReactionException("'좋아요'를 누르지 않은 한줄평입니다.");
-		}
-		comment.cancelLike();
-		commentRepository.update(comment);
-		likeRepository.deleteCommentLike(commentId, userId);
-	}
+        if (movie != null) {
+            log.info("수정 전 영화 총 평점 : {}, 수정 전 영화 한줄평 개수 : {}", movie.sumOfRating, movie.commentCount)
+        }
+        movie?.isCommentedWithRating(newComment.rating)
+        if (movie != null) {
+            log.info("수정된 영화 총 평점 : {}, 수정된 영화 한줄평 개수 : {}", movie.sumOfRating, movie.commentCount)
+        }
+        movieRepository.editMovie(movieId, movie)
+        return of(save!!, user.alias)
+    }
 
-	@Transactional
-	public void incrementCommentDislike(Long commentId, UUID userId) {
-		CommentEntity comment = commentRepository.findByCommentId(commentId)
-			.orElseThrow(() -> new NotFoundCommentException("존재하지 않는 한줄평입니다."));
-		if (dislikeRepository.isExistDislike(commentId, userId)) {
-			throw new InvalidReactionException("이미 '싫어요'를 누른 한줄평입니다.");
-		}
-		comment.isDisliked();
-		commentRepository.update(comment);
-		dislikeRepository.saveCommentDislike(commentId, userId);
-	}
+    @Transactional
+    fun deleteCommentOnMovie(movieId: String, userId: UUID, commentId: Long?) {
+        val comment = commentRepository.findByCommentId(commentId)
+            ?.orElseThrow(Supplier { NotFoundCommentException("존재하지 않는 한줄평입니다.") })
+        if (comment!!.userId != userId) {
+            throw NotCommentWriterException("한줄평 작성자가 아닙니다.")
+        }
+        if (comment.movieId != movieId) {
+            throw NotMatchMovieCommentException("해당 영화의 한줄평이 아닙니다.")
+        }
 
-	@Transactional
-	public void decrementCommentDislike(Long commentId, UUID userId) {
-		CommentEntity comment = commentRepository.findByCommentId(commentId)
-			.orElseThrow(() -> new NotFoundCommentException("존재하지 않는 한줄평입니다."));
-		if (!dislikeRepository.isExistDislike(commentId, userId)) {
-			throw new InvalidReactionException("'싫어요'를 누르지 않은 한줄평입니다.");
-		}
-		comment.cancelDislike();
-		commentRepository.update(comment);
-		dislikeRepository.deleteCommentDislike(commentId, userId);
-	}
+        commentRepository.deleteComment(commentId)
+        val movie = movieRepository.findByMovieId(movieId)
+            ?.orElseThrow(Supplier { NoMovieException("존재하는 영화가 아닙니다.") })!!
+        log.info("[MovieService.deleteCommentOnMovie] 영화 정보 수정")
+        log.info(
+            "[MovieService.deleteCommentOnMovie] before rating : {}, commentCount : {}", movie.sumOfRating,
+            movie.commentCount
+        )
+        movie.deleteComment(comment.rating)
+        log.info(
+            "[MovieService.deleteCommentOnMovie] before rating : {}, commentCount : {}", movie.sumOfRating,
+            movie.commentCount
+        )
+        movieRepository.editMovie(movieId, movie)
+    }
+
+    @Transactional
+    fun incrementCommentLike(commentId: Long?, userId: UUID?) {
+        val comment = commentRepository.findByCommentId(commentId)
+            ?.orElseThrow(Supplier { NotFoundCommentException("존재하지 않는 한줄평입니다.") })
+        if (likeRepository.isExistLike(commentId, userId)) {
+            throw InvalidReactionException("이미 '좋아요'를 누른 한줄평입니다.")
+        }
+        comment!!.isLiked()
+        commentRepository.update(comment)
+        likeRepository.saveCommentLike(commentId, userId)
+    }
+
+    @Transactional
+    fun decrementCommentLike(commentId: Long?, userId: UUID?) {
+        val comment = commentRepository.findByCommentId(commentId)
+            ?.orElseThrow(Supplier { NotFoundCommentException("존재하지 않는 한줄평입니다.") })
+        if (!likeRepository.isExistLike(commentId, userId)) {
+            throw InvalidReactionException("'좋아요'를 누르지 않은 한줄평입니다.")
+        }
+        comment!!.cancelLike()
+        commentRepository.update(comment)
+        likeRepository.deleteCommentLike(commentId, userId)
+    }
+
+    @Transactional
+    fun incrementCommentDislike(commentId: Long?, userId: UUID?) {
+        val comment = commentRepository.findByCommentId(commentId)
+            ?.orElseThrow(Supplier { NotFoundCommentException("존재하지 않는 한줄평입니다.") })
+        if (dislikeRepository.isExistDislike(commentId, userId)) {
+            throw InvalidReactionException("이미 '싫어요'를 누른 한줄평입니다.")
+        }
+        comment!!.isDisliked()
+        commentRepository.update(comment)
+        dislikeRepository.saveCommentDislike(commentId, userId)
+    }
+
+    @Transactional
+    fun decrementCommentDislike(commentId: Long, userId: UUID) {
+        val comment = commentRepository.findByCommentId(commentId)
+            ?.orElseThrow(Supplier { NotFoundCommentException("존재하지 않는 한줄평입니다.") })
+        if (!dislikeRepository.isExistDislike(commentId, userId)) {
+            throw InvalidReactionException("'싫어요'를 누르지 않은 한줄평입니다.")
+        }
+        comment!!.cancelDislike()
+        commentRepository.update(comment)
+        dislikeRepository.deleteCommentDislike(commentId, userId)
+    }
 }
--- application/src/main/java/core/application/movies/service/MovieService.kt
+++ application/src/main/java/core/application/movies/service/MovieService.kt
@@ -1,39 +1,37 @@
-package core.application.movies.service;
+package core.application.movies.service
 
-import org.springframework.data.domain.Page;
+import core.application.movies.constant.Genre
+import core.application.movies.constant.MovieSearch
+import core.application.movies.models.dto.response.MainPageMoviesRespDTO
+import core.application.movies.models.dto.response.MovieDetailRespDTO
+import core.application.movies.models.dto.response.MovieSearchRespDTO
+import org.springframework.data.domain.Page
 
-import core.application.movies.constant.Genre;
-import core.application.movies.constant.MovieSearch;
-import core.application.movies.models.dto.response.MainPageMoviesRespDTO;
-import core.application.movies.models.dto.response.MovieDetailRespDTO;
-import core.application.movies.models.dto.response.MovieSearchRespDTO;
+interface MovieService {
+    /**
+     * 메인 페이지에서 찜 많은 순 영화 10개, 평점 높은 순 영화 10개, 최신순 영화 10개의 정보를 보여준다.
+     * @return 찜 많은 순, 평점 높은 순, 최신순으로 정렬된 영화 정보
+     */
+    fun getMainPageMovieInfo(): MainPageMoviesRespDTO
 
-public interface MovieService {
+    /**
+     * 통합 검색은 검색 후 정확도순, 최신순을 제공한다.
+     *
+     * @param page 검색 페이지 목록
+     * @param sort 정렬 조건
+     * @param query 검색어
+     * @return 검색한 영화 정보
+     */
+    fun searchMovies(page: Int?, sort: MovieSearch?, query: String?): Page<MovieSearchRespDTO?>?
 
-	/**
-	 * 메인 페이지에서 찜 많은 순 영화 10개, 평점 높은 순 영화 10개, 최신순 영화 10개의 정보를 보여준다.
-	 * @return 찜 많은 순, 평점 높은 순, 최신순으로 정렬된 영화 정보
-	 */
-	public MainPageMoviesRespDTO getMainPageMovieInfo();
+    fun getMoviesWithGenreRatingOrder(page: Int?, genre: Genre?): Page<MovieSearchRespDTO?>?
 
-	/**
-	 * 통합 검색은 검색 후 정확도순, 최신순을 제공한다.
-	 *
-	 * @param page 검색 페이지 목록
-	 * @param sort 정렬 조건
-	 * @param query 검색어
-	 * @return 검색한 영화 정보
-	 */
-	public Page<MovieSearchRespDTO> searchMovies(Integer page, MovieSearch sort, String query);
+    fun getMoviesWithGenreLatestOrder(page: Int?, genre: Genre?): Page<MovieSearchRespDTO?>?
 
-	public Page<MovieSearchRespDTO> getMoviesWithGenreRatingOrder(Integer page, Genre genre);
-
-	public Page<MovieSearchRespDTO> getMoviesWithGenreLatestOrder(Integer page, Genre genre);
-
-	/**
-	 * 사용자가 영화 상세 페이지에서 필요로 하는 정보를 보내준다.
-	 * @param movieId KMDB API의 DOCID
-	 * @return 영화 상세 페이지에서 필요로하는 영화 정보
-	 */
-	public MovieDetailRespDTO getMovieDetailInfo(String movieId);
+    /**
+     * 사용자가 영화 상세 페이지에서 필요로 하는 정보를 보내준다.
+     * @param movieId KMDB API의 DOCID
+     * @return 영화 상세 페이지에서 필요로하는 영화 정보
+     */
+    fun getMovieDetailInfo(movieId: String?): MovieDetailRespDTO?
 }
