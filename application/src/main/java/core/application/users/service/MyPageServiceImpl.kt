655c60dd0ff131c288c749fa38b17d5f7d54f927
minjung <minjung0768@gmail.com>
2024-11-05 09:57:44 +0900

    bugfix : 수정 my page 생성자 주입
    생성자 주입에 문제가 생겨 서비스가 제대로 제공되지 않는 문제 수정

--- application/src/main/java/core/application/users/service/MyPageServiceImpl.kt
+++ application/src/main/java/core/application/users/service/MyPageServiceImpl.kt
@@ -11,22 +11,21 @@ import java.util.*
 
 @Service
 @RequiredArgsConstructor
-class MyPageServiceImpl : MyPageService {
-    private val dibRepo: DibRepository? = null
-    private val userRepo: UserRepository? = null
-    private val movieRepo: CachedMovieRepository? = null
+class MyPageServiceImpl (private val dibRepo: DibRepository,
+                         private val userRepo: UserRepository,
+                         private val movieRepo: CachedMovieRepository
+): MyPageService {
 
-    // 나중에 수정 -> 파라미터 제거
     override fun getMyPage(userId: UUID?): MyPageRespDTO? {
         // 현재 user 불러오기 -> 나중에 수정
 
-        val user = userRepo!!.findByUserId(userId)
+        val user = userRepo.findByUserId(userId)
 
         // user 찜 목록 불러오기
-        val myDibs = dibRepo!!.findByUserId(userId)
+        val myDibs = dibRepo.findByUserId(userId)
         val myDibDTOs: MutableList<DibDetailRespDTO> = ArrayList()
         for (myDib in myDibs) {
-            val movie = movieRepo!!.findByMovieId(myDib.movieId)
+            val movie = movieRepo.findByMovieId(myDib.movieId)
             val dibDetail: DibDetailRespDTO = DibDetailRespDTO(
                 movieId = myDib.movieId,
                 movieTitle = movie?.get()?.title,
@@ -36,14 +35,13 @@ class MyPageServiceImpl : MyPageService {
             myDibDTOs.add(dibDetail)
         }
 
-
         // DTO로 변환 -> user 정보 불러오는 로직 나중에 수정
         val myPageRespDTO = MyPageRespDTO (
-            userEmail = user.get().userEmail,
-            alias = user.get().alias,
-            phoneNum = user.get().phoneNum,
-            userName = user.get().userName,
-            role = user.get().role,
+            userEmail = user?.get()?.userEmail,
+            alias = user?.get()?.alias,
+            phoneNum = user?.get()?.phoneNum,
+            userName = user?.get()?.userName,
+            role = user?.get()?.role,
             dibDTOList = myDibDTOs
         )
 
