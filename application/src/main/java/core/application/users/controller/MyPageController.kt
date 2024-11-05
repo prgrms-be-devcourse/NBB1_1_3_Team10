7501e5b128181cff2324eba606e049d5732a0b0f
minjung <minjung0768@gmail.com>
2024-11-05 09:57:44 +0900

    bugfix : 수정 my page 생성자 주입
    생성자 주입에 문제가 생겨 서비스가 제대로 제공되지 않는 문제 수정

--- application/src/main/java/core/application/users/controller/MyPageController.kt
+++ application/src/main/java/core/application/users/controller/MyPageController.kt
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import lombok.RequiredArgsConstructor
 import lombok.extern.slf4j.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,14 +19,16 @@ import org.springframework.web.bind.annotation.RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
 @Tag(name = "MyPage", description = "마이 페이지 API")
-class MyPageController {
-    private val myPageService: MyPageService? = null
+class MyPageController
+
+@Autowired
+constructor(private val myPageService: MyPageService) {
 
     @Operation(summary = "유저 마이페이지")
     @GetMapping("/mypage")
     fun getMyPage(@AuthenticationPrincipal userDetails: CustomUserDetails): ApiResponse<MyPageRespDTO?> {
         val userId = userDetails.userId
-        val myPage = myPageService!!.getMyPage(userId)
+        val myPage = myPageService.getMyPage(userId)
         return ApiResponse.onSuccess(myPage)
     }
 }
